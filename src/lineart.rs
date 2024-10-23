afde97dde811d1bd94d60c43112cd5c46a4fb159
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-10-23 20:38:37 +0200

    Function to change the midpoint of the grayscale
    This does not work as expected

--- src/lineart.rs
+++ src/lineart.rs
@@ -55,3 +55,70 @@ pub(crate) fn sobel_blend_dodge(image: PhotonImage) -> PhotonImage {
     blend(&mut base_layer, &sobel, "dodge");
     base_layer
 }
+
+/// Changes the midpoint of the grayscale from 122 to the new midpooint
+/// Tends to darken the dark lines if midpoint is more than 127
+/// Tends to erase the dark lines if midpoint is less than 127
+/// What it does is assign the range 0-127 to 0-new_midpoint and 127-255 to new_midpoint-255
+/// This is done by a linear correlation
+/// Only the RGB values are changed, the alpha is left untouched
+/// Note that you can give this function a non-greyscale value, but this function assumes R=G=B and will take the value of the red channel for all the other colour channels
+pub(crate) fn change_grayscale_range_midpoint(
+    image: PhotonImage,
+    new_midpoint: u8,
+) -> Result<PhotonImage> {
+    fn new_low_range(x_value: u8, new_midpoint: u8) -> u8 {
+        // we make a linear correlation y = ax + b
+        // using (x1, y1) = (0,0) and (x2, y2) = (127, new_midpoint)
+        // classic formula gives:
+        // y = y1 + (y2 - y1)/(x2 - x1)*(x - x1)
+        assert!(x_value <= new_midpoint);
+        let x1 = 0;
+        let y1 = 0;
+        let x2 = 127;
+        let y2 = new_midpoint;
+        let a = ((y2 - y1) as f32) / ((x2 - x1) as f32);
+        y1 + ((a * ((x2 - x1) as f32)) as u8)
+    }
+    fn new_high_range(x_value: u8, new_midpoint: u8) -> u8 {
+        // we make a linear correlation y = ax + b
+        // using (x1, y1) = (127, new_midpoint) and (x2, y2) = (255, 255)
+        // classic formula gives:
+        // y = y1 + (y2 - y1)/(x2 - x1)*(x - x1)
+        assert!(x_value >= new_midpoint);
+        let x1 = 127;
+        let y1 = new_midpoint;
+        let x2 = 255;
+        let y2 = 255;
+        let a = ((y2 - y1) as f32) / ((x2 - x1) as f32);
+        y1 + ((a * ((x2 - x1) as f32)) as u8)
+    }
+    let width = image.get_width();
+    let height = image.get_height();
+
+    let raw_pixels = image.get_raw_pixels();
+    let mut new_pixels = Vec::with_capacity(raw_pixels.len());
+    // cannot just image.get_raw_pixels().chunks_exact() because of temporary value dropped while borrowed
+    let mut pixel_iter = raw_pixels.chunks_exact(4);
+    // using chunks of 4 because of RGBA format
+    for pixel in pixel_iter.by_ref() {
+        let grey_value = *pixel
+            .first()
+            .context("The current pixel doesn't have any value inside")?;
+        let alpha = *pixel
+            .get(3)
+            .context("The current pixel doesn't have an alpha value")?;
+        let new_gray = if grey_value <= new_midpoint {
+            new_low_range(grey_value, new_midpoint)
+        } else {
+            new_high_range(grey_value, new_midpoint)
+        };
+        // keep the same alpha
+        let new_pixel = vec![new_gray, new_gray, new_gray, alpha];
+        new_pixels.extend(new_pixel);
+    }
+    // just in case the size was actually not divisible
+    new_pixels.extend(pixel_iter.remainder().to_vec());
+
+    Ok(PhotonImage::new(new_pixels, width, height))
+}
