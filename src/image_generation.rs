ebc1462fa112ff49ac418cba4bff050da006acc1
Turtyo <49365680+Turtyo@users.noreply.github.com>
2025-01-07 20:49:25 +0100

    Finish the image grid generation

--- src/image_generation.rs
+++ src/image_generation.rs
@@ -1,8 +1,15 @@
 use std::path::Path;
 
 use crate::lineart::{self, Method};
+use ab_glyph::FontRef;
 use anyhow::{Context, Result};
-use photon_rs::{multiple::blend, native::save_image, PhotonImage};
+use image::{ExtendedColorType, ImageBuffer, ImageFormat, Rgba};
+use imageproc::drawing::draw_text_mut;
+use photon_rs::{
+    multiple::blend,
+    native::{open_image, save_image},
+    PhotonImage,
+};
 
 pub(crate) fn generate_all_images(
     base_image: PhotonImage,
@@ -29,40 +36,150 @@ pub(crate) fn generate_all_images(
             blend(&mut image, &original_image, "multiply")
         }
         for darken_index in 0..(darken_number - 1) {
-            let mut save_path = output_path.as_ref().to_owned();
-            save_path.push(format!(
-                "blur_{}_darken_{}",
+            let save_path = build_image_path(
+                &output_path,
                 blur_radius,
-                min_darken_number + darken_index * darken_step
-            ));
-            save_path.set_extension("png");
-            let save_path = save_path.to_str().with_context(|| {
-                format!(
-                    "The path to save the image cannot be converted to a string: {:?}",
-                    save_path
-                )
-            })?;
-            save_image(image.clone(), save_path)?;
+                min_darken_number + darken_index * darken_step,
+            )?;
+            println!("{}", save_path);
+            save_image(image.clone(), save_path.as_str())?;
             for _ in 0..darken_step {
                 blend(&mut image, &original_image, "multiply")
             }
         }
-        let mut save_path = output_path.as_ref().to_owned();
-        save_path.push(format!(
-            "blur_{}_darken_{}",
+        let save_path = build_image_path(
+            &output_path,
             blur_radius,
-            min_darken_number + (darken_number -1) * darken_step
-        ));
-        save_path.set_extension("png");
-        
-        let save_path = save_path.to_str().with_context(|| {
-            format!(
-                "The path to save the image cannot be converted to a string: {:?}",
-                save_path
-            )
-        })?;
-        
-        save_image(image, save_path)?; // save image for last iteration
+            min_darken_number + (darken_number - 1) * darken_step,
+        )?;
+
+        save_image(image, save_path.as_str())?; // save image for last iteration
     }
+    Ok(())
+}
+
+fn build_image_path(output_path: impl AsRef<Path>, blur: i32, darken: u8) -> Result<String> {
+    let mut save_path = output_path.as_ref().to_owned();
+    save_path.push(format!("blur_{}_darken_{:02}", blur, darken));
+    save_path.set_extension("png");
+    let save_path = save_path.to_str().with_context(|| {
+        format!(
+            "The path to save the image cannot be converted to a string: {:?}",
+            save_path
+        )
+    })?;
+    Ok(save_path.to_string())
+}
+
+pub(crate) fn generate_image_grid(
+    min_blur_radius: i32,
+    blur_step: i32,
+    blur_number: u8,
+    min_darken_number: u8,
+    darken_step: u8,
+    darken_number: u8,
+    input_path: impl AsRef<Path>,
+    output_path: impl AsRef<Path>,
+) -> Result<()> {
+    let right_padding_mult: f32 = 1.2;
+    let down_padding_mult: f32 = 1.1;
+    let top_padding_mult: f32 = 0.6;
+    let left_padding_mult: f32 = 1.3;
+
+    //load a first image to get the dimensions and extrapolate the size of the final image
+    let first_image_path = build_image_path(&input_path, min_blur_radius, min_darken_number)?;
+    let first_image = open_image(first_image_path.as_str())?;
+    let first_width = first_image.get_width();
+    let first_height = first_image.get_height();
+    let left_padding = (first_width as f32) * left_padding_mult;
+    let top_padding = (first_height as f32) * top_padding_mult;
+    let total_width =
+        (first_width as f32 * right_padding_mult) * (darken_number as f32) + left_padding; // darken by rows
+    let total_height =
+        (first_height as f32 * down_padding_mult) * (blur_number as f32) + top_padding; // blur by columns
+    let total_width = total_width as u32;
+    let total_height = total_height as u32;
+
+    // data for the text
+    let pixel_to_repeat: Rgba<u8> = Rgba([255, 255, 255, 255]);
+    let mut canvas = ImageBuffer::from_pixel(total_width, total_height, pixel_to_repeat);
+    let font = FontRef::try_from_slice(include_bytes!("../fonts/Exo2-Light.otf"))?;
+    let text_color = Rgba([0_u8, 0_u8, 0_u8, 255_u8]);
+    let scale = (first_width as f32) / 3_f32;
+    
+    // constant positions for the text
+    let blur_text_x = (left_padding / 2_f32) as i32;
+    let darken_text_position_y = top_padding as i32 - (first_height as f32 / 3_f32) as i32;
+    
+    for blur_index in 0..blur_number {
+        let blur_radius = min_blur_radius + (blur_index as i32 * blur_step);
+        let image_y =
+            ((first_height as f32 * down_padding_mult) * (blur_index as f32) + top_padding) as i64;
+        if blur_index == 0 {
+            draw_text_mut(
+                &mut canvas,
+                text_color,
+                0,
+                image_y as i32,
+                scale,
+                &font,
+                "Blur",
+            );
+            
+            let darken_text_position_x = (first_width as f32 / 3_f32) as i32;
+            draw_text_mut(
+                &mut canvas,
+                text_color,
+                darken_text_position_x,
+                darken_text_position_y,
+                scale,
+                &font,
+                "Darken",
+            );
+        }
+        let blur_text_y = image_y as i32 + (first_height as f32 / 2_f32) as i32;
+
+        draw_text_mut(
+            &mut canvas,
+            text_color,
+            blur_text_x,
+            blur_text_y,
+            scale,
+            &font,
+            format!("{}", blur_radius).as_str(),
+        );
+
+        for darken_index in 0..darken_number {
+            let darken = min_darken_number + darken_index * darken_step;
+            let fetch_path = build_image_path(&input_path, blur_radius, darken)?;
+            let image = image::ImageReader::open(fetch_path)?.decode()?;
+            let image_x =
+                (first_width as f32 * right_padding_mult) * (darken_index as f32) + left_padding;
+            image::imageops::overlay(&mut canvas, &image, image_x as i64, image_y);
+            
+            if blur_index == 0 {
+                let darken_text_position_x = image_x as i32 + (first_width as f32 / 2_f32) as i32;
+                draw_text_mut(
+                    &mut canvas,
+                    text_color,
+                    darken_text_position_x,
+                    darken_text_position_y,
+                    scale,
+                    &font,
+                    format!("{}", darken).as_str(),
+                );
+            }
+            
+        }
+    }
+
+    image::save_buffer_with_format(
+        output_path,
+        &canvas,
+        total_width,
+        total_height,
+        ExtendedColorType::Rgba8,
+        ImageFormat::Png,
+    )?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod image_generation;
 mod lineart;
 
+use image_generation::generate_image_grid;
 use lineart::Method;
 use photon_rs::native::{open_image, save_image};
 
@@ -40,16 +41,35 @@ fn main() {
     save_image(lineart_image, "result_images/sacquet_01.png").expect("Couldn't save image");
     save_image(lineart_image_2, "result_images/sacquet_02.png").expect("Couldn't save image");
 
+    let min_blur_radius = 1;
+    let blur_step = 1;
+    let blur_number = 5;
+    let min_darken_number = 0;
+    let darken_step = 1;
+    let darken_number = 4;
+
     image_generation::generate_all_images(
         image,
-        1,
-        1,
-        5,
-        0,
-        1,
-        4,
+        min_blur_radius,
+        blur_step,
+        blur_number,
+        min_darken_number,
+        darken_step,
+        darken_number,
         Method::SobelBlendDodge,
         "multiple_images/sacquet",
     )
     .unwrap();
+
+    generate_image_grid(
+        min_blur_radius,
+        blur_step,
+        blur_number,
+        min_darken_number,
+        darken_step,
+        darken_number,
+        "multiple_images/sacquet",
+        "./canvas.png",
+    )
+    .expect("Couldn't generate the image grid");
 }
